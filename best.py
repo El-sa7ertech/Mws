2f95356f123bcabb657245133c54e50744e157a3
El-sa7ertech <aminabdalbdea10@gmail.com>
2026-04-10 20:38:59 +0200

    Add files via upload

---
+++ best.py
@@ -0,0 +1,48 @@
+import os
+import asyncio
+from flask import Flask
+from telethon import TelegramClient
+
+# ======================
+# إعداد Telegram
+# ======================
+api_id = 123456          # حط api_id
+api_hash = "your_api_hash"  # حط api_hash
+bot_username = "mysudan1bot"  # يوزر البوت
+
+client = TelegramClient("session", api_id, api_hash)
+
+# ======================
+# Flask
+# ======================
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Telegram Bot Running ✅"
+
+# ======================
+# تشغيل Telegram
+# ======================
+async def start_bot():
+"""  await client.start()
+    print("✅ Telegram Connected", flush=True)
+
+    # إرسال رسالة تجريبية عند التشغيل
+    await client.send_message(bot_username, "🚀 Bot is now online!")
+"""
+await client.start()
+    user = "Aminabdalbdea"
+    msg = "اشتغل"
+    await client.send_message(user, msg)
+    print("تم إرسال الرسالة!")
+
+# ======================
+# تشغيل السيرفر
+# ======================
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
+
+    PORT = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=PORT)
