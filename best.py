cfdd2c04be3ed047bf818cc45c34b75b72e88151
El-sa7ertech <aminabdalbdea10@gmail.com>
2026-04-10 18:22:01 +0200

    Update best.py

--- best.py
+++ best.py
@@ -1,1 +1,19 @@
-print("helo world amin")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# الصفحة الرئيسية
+@app.route("/")
+def home():
+    return "Hello World from Render 🚀"
+
+# Webhook تجريبي
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    return "OK", 200
+
+# تشغيل السيرفر
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=PORT)
