e55eb517825ebe404e7b6860451a80e9a465c831
El-sa7ertech <aminabdalbdea10@gmail.com>
2026-04-11 01:57:45 +0200

    Initialize Telegram bot with Telethon

---
+++ tl.py
@@ -0,0 +1,20 @@
+import asyncio
+import os
+from telethon import TelegramClient
+
+api_id = int(os.environ.get("api_id"))
+api_hash = os.environ.get("api_hash")
+
+client = TelegramClient("session", api_id, api_hash)
+
+async def main():
+    print("🚀 Telegram starting...")
+
+    await client.start()
+    print("✅ Telegram Connected")
+
+    await client.send_message("me", "Bot started 🚀")
+
+    await client.run_until_disconnected()
+
+asyncio.run(main())
