2076d67ebcfb35a6496b9cb17e226bc1cc5b0431
El-sa7ertech <aminabdalbdea10@gmail.com>
2026-04-11 08:25:29 +0200

    Refactor bot.py to integrate Flask and Telegram

--- bot.py
+++ bot.py
@@ -1,17 +1,70 @@
 import asyncio
+from flask import Flask, request
 from telethon import TelegramClient, events
+from threading import Thread
+import os
 
-api_id = int(os.environ.get("api_id"))
-api_hash = os.environ.get("api_hash")
+print("9")
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+
 client = TelegramClient("session", api_id, api_hash)
 
+print(api_id, api_hash)
 
+app = Flask(__name__)
 
-async def main():
-    
+print(15)
+
+# ======================
+# Telegram loop
+# ======================
+tg_loop = asyncio.new_event_loop()
+
+async def start_bot():
+    print("🚀 start_bot called", flush=True)
+
     await client.start()
-    await client.send_message("Aminabdalbdea", "اشتغل")
-    print("Bot is running...")
+    print("✅ Telegram Connected", flush=True)
+
+    @client.on(events.NewMessage)
+    async def handler(event):
+        print("📩", event.text, flush=True)
+
     await client.run_until_disconnected()
 
-asyncio.run(main())
+def run_telegram():
+    print("🔥 Thread started", flush=True)
+
+    asyncio.set_event_loop(tg_loop)
+    tg_loop.run_until_complete(start_bot())
+
+# ======================
+# Flask
+# ======================
+@app.route("/")
+def home():
+    return "Bot is running ✅"
+
+@app.route("/send")
+def send():
+    text = request.args.get("msg", "Hello")
+
+    asyncio.run_coroutine_threadsafe(
+        client.send_message("Aminabdalbdea", "00000"),
+        tg_loop
+    )
+
+    return "Sent"
+
+# ======================
+# MAIN
+# ======================
+if __name__ == "__main__":
+    Thread(target=run_telegram, daemon=True).start()
+
+    print("🌐 Flask starting...", flush=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
